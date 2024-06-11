127a9b520402f15b748d3e63186faaf6bcc6a385
Vitaly05 <vitalik050405@gmail.com>
2024-06-11 22:38:21 +0300

    Added vertical scroll script

--- resources/js/modules/horizontal-scroll.js
+++ resources/js/modules/horizontal-scroll.js
@@ -27,7 +27,41 @@ export default function horizontalScroll (parentElement) {
 
 	if (parentElement) {
 		$(parentElement).find('.js-horizontal-scroll').each(function () {
-			$(this).hScroll(60);
+			$(this).hScroll(20);
+		});
+	}
+}
+
+export function verticalScroll (parentElement) {
+	if (!$.fn.vScroll) {
+		// vertical scroll
+		$.fn.vScroll = function (amount) {
+			const $target = $(this);
+			amount = amount || 120;
+
+			function verticalScroll (event) {
+				event.preventDefault();
+
+				const originalEvent = event.originalEvent;
+				const direction = originalEvent.detail ? originalEvent.detail * -amount : originalEvent.wheelDelta;
+				let position = $target.scrollTop();
+
+				position += direction > 0 ? -amount : amount;
+
+				$target.scrollTop(position);
+			}
+
+			$target.off("DOMMouseScroll mousewheel");
+
+			// if($target[0].scrollHeight > $target[0].clientHeight) {
+				$target.on("DOMMouseScroll mousewheel", verticalScroll);
+			// }
+		};
+	}
+
+	if (parentElement) {
+		$(parentElement).find('.js-vertical-scroll').each(function () {
+			$(this).vScroll(20);
 		});
 	}
 }
